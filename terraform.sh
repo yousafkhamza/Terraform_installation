32a2d03ca5831a1582577965af5aa6427e24ee1c
Yousaf K Hamza <31238282+yousafkhamza@users.noreply.github.com>
2021-10-24 00:15:48 +0530

    Update terraform.sh

--- terraform.sh
+++ terraform.sh
@@ -18,7 +18,7 @@ if [ $? = 0 ]; then
     echo "-----------"
     terraform -v
     echo "-----------"
-    cvcheck=$(terraform -v | grep "out of date")
+    terraform -v | grep "out of date" >/dev/null 2>&1
     if [ $? = 0 ]; then
             read -p "Terraform version is out dated so do you need to update the version Y/N: " con
             if [[ "$con" =~ ^([yY][eE][sS]|[yY])+$ ]]; then
