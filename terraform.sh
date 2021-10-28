e0111a9faaab5ab77ddc3894346711ca0cd09744
Yousaf K Hamza <31238282+yousafkhamza@users.noreply.github.com>
2021-10-28 14:03:58 +0530

    Working now

--- terraform.sh
+++ terraform.sh
@@ -7,14 +7,14 @@ function terraform_installation () {
     echo "Downloading Terraform From Hashicorp ............."
     sudo rm -f /tmp/terraform_*.zip
     wget --no-check-certificate $(curl -s https://www.terraform.io/downloads.html | grep linux_amd | grep -Eo 'href="[^\"]+"' | cut -d= -f2 | sed 's/["]//g') -P /tmp/ >/dev/null 2>&1
-    if [ $? = 0 ]; then
-        unzip /tmp/terraform_*.zip
-        sudo mv terraform /usr/bin/
-        sudo rm -f /tmp/terraform_*.zip
-   else
-        echo "Terraform Downloading is failed.... So, please be try the same via manually"
-        echo "https://www.terraform.io/downloads.html"
-   fi
+        if [ $? = 0 ]; then
+            unzip /tmp/terraform_*.zip
+            sudo mv terraform /usr/bin/
+            sudo rm -f /tmp/terraform_*.zip
+        else
+            echo "Terraform Downloading is failed.... So, please be try the same via manually"
+            echo "https://www.terraform.io/downloads.html"
+        fi
 }
 
 which terraform >/dev/null 2>&1
@@ -25,18 +25,17 @@ if [ $? = 0 ]; then
     terraform -v
     echo "-----------"
     terraform -v | grep "out of date" >/dev/null 2>&1
-    if [[ "$?" = 0 ]]; then
-            echo "You're terraform version is out dated so we are going to update automatically...."
-            echo "Updation starts in 10sec. if you don't needs to update then please exit the script using [ctrl + c]...."
-            for ((i=0;i<10;i++)); do sleep 1; printf "$i..\n"; done
-            echo "Updation begins...."
-            echo "Removing the current version of terraform...."; sudo rm -r $(which terraform)
-            terraform_installation
-            echo "Terrafrom Updation is successful. Current version is given below......"
-            echo "-----------"
-            terraform -v
-            echo "-----------"
-        fi
+    if [ $? = 0 ]; then
+        echo "You're terraform version is out dated so we are going to update automatically...."
+        echo "Updation starts in 10sec. if you don't needs to update then please exit the script using [ctrl + c]...."
+        for ((i=0;i<10;i++)); do sleep 1; printf "$i..\n"; done
+        echo "Updation begins...."
+        echo "Removing the current version of terraform...."; sudo rm -r $(which terraform)
+        terraform_installation
+        echo "Terrafrom Updation is successful. Current version is given below......"
+        echo "-----------"
+        terraform -v
+        echo "-----------"
     fi
     sleep 1
     exit 1
